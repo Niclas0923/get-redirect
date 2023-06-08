dd5d55999acd649e69f139cfd902896a21e1cf1e
Niclas0923 <736012843@qq.com>
2023-06-09 00:55:14 +0800

    新增登陆功能监听，即将制作网页来管理

--- mod/server.js
+++ mod/server.js
@@ -1,13 +1,29 @@
 const server = (options,userRoutes,data,httpsO=false)=>{
     const express = require("express")
+    const bodyParser = require("body-parser");
+    const fs = require("fs")
     // 开启监听测试
     const app = express();
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
     app.use('/', userRoutes);
+    app.use(express.static('./page'))
+
+    app.post("/login",(req, res)=>{
+        const {name,password} = req.body
+        const usr = JSON.parse(String(fs.readFileSync("./config/user.json")))
+        const users = []
+        for (const i in usr) users.push(usr[i][0])
+        const index = users.indexOf(name)
+        if (index !== -1){
+            if (usr[index][1] === password) res.send(["密码正确"])
+            else res.send(["密码错误"])
+        }else res.send(["用户名不存在"])
+    })
 
     // 判断协议
     if (httpsO){
         const https = require("https")
-        const fs = require("fs")
         const option = {
             key: fs.readFileSync(options["ssl"]["key"]),
             cert: fs.readFileSync(options["ssl"]["cert"])
