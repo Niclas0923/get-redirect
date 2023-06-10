7d8adf874bc2a64a2c5aaa4f8f011a47ca491d32
Niclas0923 <736012843@qq.com>
2023-06-10 14:16:46 +0800

    直接加载改为转跳到新窗口

--- page/class/Main.js
+++ page/class/Main.js
@@ -40,7 +40,7 @@ class Main {
             val+=`
             <div class="card-page">
                 <h4>${i[0]}</h4>
-                <a href="${i[1]}">${i[1]}</a>
+                <a href="${i[1]}" target="_blank">${i[1]}</a>
                 <a class="false" href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
                         <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
