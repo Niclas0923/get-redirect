b05b7fb8a8ce8422b40456fd2a384f48d816dd3f
Niclas0923 <736012843@qq.com>
2023-07-21 03:31:27 +0800

    设置提示信息，修改log显示内容。

--- page/src/components/PageLog.vue
+++ page/src/components/PageLog.vue
@@ -8,26 +8,29 @@
         leave-active-class="animate__bounceOut"
     >
       <div class="row" v-for="i in nLog" :key="i.id">
-      <div class="col-md-2 d-flex justify-content-center align-items-center app-line">
-        <span class="badge rounded-pill bg-primary">{{i.time}}</span>
-      </div>
-      <div class="col-md-10">
-        <div class="card rounded-0 border-0 shadow my-4">
-          <div class="card-body">
-            <h5 class="card-title" v-if="i.thing === 'addOne'">添加项目</h5>
-            <h5 class="card-title" v-if="i.thing === 'removeOne'">删除项目</h5>
-            <p class="card-text" v-if="i.thing === 'addOne'">
-              管理员<mark>{{i.who}}</mark>增添了项目<mark>/{{i.obj.name}} ——> {{i.obj.url}}</mark>。
-            </p>
-            <p class="card-text" v-if="i.thing === 'removeOne'">
-              管理员<mark>{{i.who}}</mark>删除了项目<mark>/{{i.obj.name}} ——> {{i.obj.url}}</mark>，
-              它是管理员<mark>{{i.obj.userName}}</mark>于{{i.obj.time}}创建的。
-            </p>
+        <div class="col-md-2 d-flex justify-content-center align-items-center app-line">
+          <span class="badge rounded-pill bg-primary">{{i.time}}</span>
+        </div>
+        <div class="col-md-10">
+          <div class="card rounded-0 border-0 shadow my-4">
+            <div class="card-body">
+              <h5 class="card-title" v-if="i.thing === 'addOne'">添加</h5>
+              <h5 class="card-title" v-if="i.thing === 'removeOne'">删除</h5>
+              <p class="card-text" v-if="i.thing === 'addOne'">
+                管理员<mark>{{i.who}}</mark>增添了项目<mark>/{{i.obj.name}} ——> {{i.obj.url}}</mark>。
+              </p>
+              <p class="card-text" v-if="i.thing === 'removeOne'">
+                管理员<mark>{{i.who}}</mark>删除了项目<mark>/{{i.obj.name}} ——> {{i.obj.url}}</mark>，
+                它是管理员<mark>{{i.obj.userName}}</mark>于{{i.obj.time}}创建的。
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </transition-group>
+    <div v-if="log.length === 0">
+      <h2>当前没有log信息。</h2>
+    </div>
   </div>
 </div>
 </template>
