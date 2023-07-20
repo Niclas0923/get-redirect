b15251e65bd7e961a2f098be5dcc3b1231426a65
Niclas0923 <736012843@qq.com>
2023-07-21 03:03:17 +0800

    给时间属性新增id属性

--- mod/whiteLog.js
+++ mod/whiteLog.js
@@ -1,11 +1,14 @@
+const shortid = require("shortid");
 const whiteLog = (name,time,obj,thing)=>{
     const fs = require("fs")
     const logs = JSON.parse(String(fs.readFileSync("./config/log.json")))
+    const id = shortid.generate();
     logs.push({
         who:name,
         time,
         obj,
-        thing
+        thing,
+        id
     })
     fs.writeFileSync("./config/log.json",JSON.stringify(logs))
 }
