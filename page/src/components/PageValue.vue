ea654a012dfdf3459f58bcadf08c17d4e6082541
Niclas0923 <736012843@qq.com>
2023-07-20 10:01:21 +0800

    新增添加按钮和动画

--- page/src/components/PageValue.vue
+++ page/src/components/PageValue.vue
@@ -1,8 +1,27 @@
 <template>
   <div class="container col-12 offset-0 col-md-10 offset-md-1 col-lg-8 offset-lg-2">
-    <PageValueAdd
-        :allTags="allTags"
-    />
+<!--    添加按钮 -->
+    <transition
+        appear
+        name="animate__animated animate__bounce"
+        enter-active-class="animate__bounceInUp"
+        leave-active-class="animate__bounceOut"
+    >
+      <div class="card-page add-div" v-show="!addOn" @click="addOn=!addOn">
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plus add-svg" viewBox="0 0 16 16">
+          <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+        </svg>
+      </div>
+    </transition>
+    <transition
+        name="animate__animated animate__bounce"
+        enter-active-class="animate__zoomInDown"
+    >
+      <PageValueAdd
+          v-show="addOn"
+          :allTags="allTags"
+      />
+    </transition>
     <PageValueCards
         :list="list"
     />
@@ -10,11 +29,17 @@
 </template>
 
 <script>
+import "animate.css"
 import PageValueAdd from "@/components/PageValueAdd.vue";
 import PageValueCards from "@/components/PageValueCards.vue";
 export default {
   name: 'PageValue',
   props: ["list"],
+  data(){
+    return {
+      addOn:false
+    }
+  },
   components:{
     PageValueAdd,
     PageValueCards
@@ -32,6 +57,22 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style scoped lang="less">
+.add-div{
+  position: relative;
+  padding: 25px;
+}
+.add-svg{
+  transform: translate(-50%,-50%);
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  color:gray;
+}
+.add-div:hover{
+  background-color: rgba(128, 128, 128, 0.96);
+  .add-svg{
+    color:white;
+  }
+}
 </style>
