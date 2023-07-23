f1a860d01bf2c9a77ec25384d9934d22901c163c
Niclas0923 <736012843@qq.com>
2023-07-23 14:09:22 +0800

    完成了搜索框的制作

--- page/src/components/PageList.vue
+++ page/src/components/PageList.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="container col-12 offset-0 col-md-10 offset-md-1 col-lg-8 offset-lg-2">
+    <PageListSearch
+        :valueChange="valueChange"
+    />
     <!-- 信息列表 -->
     <PageListCards
-        :list="list"
+        :list="listOn"
         :addClick="addClick"
     />
     <!-- 添加菜单 -->
@@ -17,15 +20,18 @@
 import "animate.css"
 import PageListAddForm from "@/components/PageListAddForm.vue";
 import PageListCards from "@/components/PageListCards.vue";
+import PageListSearch from "@/components/PageListSearch.vue";
 export default {
   name: 'PageList',
   props: ["list"],
   data(){
     return {
-      addOn:false
+      addOn:false,
+      searchValue:""
     }
   },
   components:{
+    PageListSearch,
     PageListAddForm,
     PageListCards
   },
@@ -36,11 +42,28 @@ export default {
         data.push(i.name)
       }
       return data
+    },
+    listOn(){
+      if (this.searchValue === ""){
+        return this.list
+      }else {
+        // 用于判断2是否在1内，不区分大小写
+        const x = (str,str2)=>{
+          return str.toLowerCase().includes(str2.toLowerCase())
+        }
+        const val = this.searchValue
+        return this.list.filter((i)=>{
+          return (x(i.name,val) || x(i.url,val) || x(i.time,val) || x(i.userName,val))
+        })
+      }
     }
   },
   methods:{
     addClick(){
       this.addOn = true
+    },
+    valueChange(val){
+      this.searchValue = val
     }
   }
 }
---
+++ page/src/components/PageListSearch.vue
@@ -0,0 +1,62 @@
+<template>
+  <transition
+      appear
+      name="animate__animated animate__bounce"
+      enter-active-class="animate__bounceInUp"
+      leave-active-class="animate__bounceOut"
+  >
+    <div class="col-12 offset-0 col-md-6 offset-md-3 div-input">
+      <input type="text" class="input" v-model.trim="searchValue"/>
+      <a class="clear" href="#" @click="searchValue=''">
+        <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16" v-pre>
+          <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+        </svg>
+      </a>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name:"PageListSearch",
+  data(){
+    return{
+      searchValue:""
+    }
+  },
+  props:["valueChange"],
+  watch:{
+    searchValue(a){
+      this.valueChange(a)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.div-input{
+  position: relative;
+  margin-top: 24px;
+  padding: 0;
+}
+.input{
+  width: 100%;
+  height: 40px;
+  box-shadow: 0 0 5px #bbbbbb;
+  padding: 0 15px;
+  border-radius: 6px;
+  border: none;
+  outline: 2px solid white;
+  outline-offset: -2px;
+}
+.input:focus {
+  outline:2px solid lightgrey;
+}
+.clear{
+  position: absolute;
+  color:gray;
+  top: 48%;
+  right: 10px;
+  transform: translate(0,-50%);
+}
+</style>
