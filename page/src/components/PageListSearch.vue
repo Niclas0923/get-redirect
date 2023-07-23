4a03afb3717dd5af390d7230f355095acfb7a471
Niclas0923 <736012843@qq.com>
2023-07-24 02:15:39 +0800

    简单设置下显示并直接编译

--- page/src/components/PageListSearch.vue
+++ page/src/components/PageListSearch.vue
@@ -8,7 +8,7 @@
 <!--      输入框 -->
       <input type="text" class="input" v-model.trim="searchValue"/>
 <!--      删除按钮 -->
-      <a href="#" @click="searchValue=''">
+      <a href="#" @click="searchValue=''" v-show="searchValue.length">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg clear" viewBox="0 0 16 16" v-pre>
           <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
         </svg>
