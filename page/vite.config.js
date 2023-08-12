fbbd5b4a60e337f3ced3720c4de1bd3c81d22c31
Niclas0923 <736012843@qq.com>
2023-08-12 16:29:08 +0800

    允许远程访问

--- page/vite.config.js
+++ page/vite.config.js
@@ -8,6 +8,7 @@ const point = JSON.parse(String(fs.readFileSync("./../config/server.json")))["ht
 export default defineConfig({
   plugins: [vue()],
   server:{
+    host:'0.0.0.0',
     port:8082,
     proxy:{
       // 设置使用的代理，默认用 http 协议
