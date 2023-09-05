cb7486997a96d0c491d138eb17e1aaa0e3ca57c9
Niclas0923 <736012843@qq.com>
2023-09-05 21:41:45 +0800

    设置本地存储

--- page/src/components/page/list/PageListAlert.vue
+++ page/src/components/page/list/PageListAlert.vue
@@ -1,24 +1,36 @@
 <script setup>
 // 记录是否显示信息
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 
-let showValue = ref(true)
+let closeAlert = ref(true)
 
 // 关闭的功能
 function close(){
-  showValue.value = false
+  closeAlert.value = true
+  window.localStorage.setItem("PageValueAlert-close-alert",JSON.stringify({close:true}))
 }
+
+// 读取本地信息的测试
+onMounted(()=>{
+  const val = JSON.parse(String(window.localStorage.getItem("PageValueAlert-close-alert")))
+  console.log(val)
+  if (val){
+    closeAlert.value = val.close
+  }else{
+    closeAlert.value = false
+    window.localStorage.setItem("PageValueAlert-close-alert",JSON.stringify({close:false}))
+  }
+})
 
 </script>
 
 <template>
   <transition
-      appear
       name="animate__animated animate__bounce"
       enter-active-class="animate__bounceInUp"
       leave-active-class="animate__bounceOut"
   >
-    <div class="alert alert-primary alert-dismissible fade show" v-if="showValue">
+    <div class="alert alert-primary alert-dismissible fade show" v-show="!closeAlert">
       <ul class="mb-0">
         <li>可以点击上方的卡片中的链接直接打开于新页面。</li>
         <li>可以通过访问同网址/api/索引来直接跳转到目标地址。</li>
