d4bd88e59bb73c64ff04d8e2b4d3cd561b2ef59e
Niclas0923 <736012843@qq.com>
2023-09-05 21:31:12 +0800

    制作信息关闭的功能

--- page/src/components/page/list/PageListAlert.vue
+++ page/src/components/page/list/PageListAlert.vue
@@ -4,6 +4,11 @@ import {ref} from "vue";
 
 let showValue = ref(true)
 
+// 关闭的功能
+function close(){
+  showValue.value = false
+}
+
 </script>
 
 <template>
@@ -19,7 +24,7 @@ let showValue = ref(true)
         <li>可以通过访问同网址/api/索引来直接跳转到目标地址。</li>
         <li>地址示例：<a href="不可点击哦" onclick="alert('不可点击哦');return false;">http(s)://test.com/api/test</a></li>
       </ul>
-      <!--        <button type="button" class="btn-close"></button>-->
+      <button type="button" class="btn-close" @click="close"></button>
     </div>
   </transition>
 </template>
